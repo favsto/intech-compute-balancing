58c02d8e3f9c13399571e4802f60647b1035e6d5
fausto_fusaro <fausto.fusaro@injenia.it>
2017-03-07 12:27:46 +0100

    get configuration from metadata instead of manual coding

--- init/env.sh
+++ init/env.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # custom variables
-echo DESTINATION_BUCKET="[bucket_name]" >> config
-echo SQL_USERNAME="[your_sql_username]" >> config
-echo SQL_PASSWORD="[your_sql_password]" >> config
+echo SQL_USERNAME=$(curl -s "http://metadata/computeMetadata/v1/instance/attributes/sql-username" \
+    -H "Metadata-Flavor: Google") >> config
+echo SQL_PASSWORD=$(curl -s "http://metadata/computeMetadata/v1/instance/attributes/sql-password" \
+    -H "Metadata-Flavor: Google") >> config
+echo DESTINATION_BUCKET=$(curl -s "http://metadata/computeMetadata/v1/instance/attributes/destination-bucket" \
+    -H "Metadata-Flavor: Google") >> config
 echo SQL_CONNECTION_NAME=$(curl -s "http://metadata/computeMetadata/v1/instance/attributes/sql-connection-name" \
     -H "Metadata-Flavor: Google") >> config
 
