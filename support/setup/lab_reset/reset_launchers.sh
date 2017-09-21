cadf16cb2f115123c12c8f7614a037cf4fac1882
fausto_fusaro <fausto.fusaro@injenia.it>
2017-09-21 10:48:10 +0200

    improvements to launchers

--- support/setup/lab_reset/reset_launchers.sh
+++ support/setup/lab_reset/reset_launchers.sh
@@ -4,10 +4,24 @@ gcloud compute instances delete launcher-eu --zone europe-west3-a --quiet
 gcloud compute instances create "launcher-eu" --description "This instance is for calling your services from Europe" --zone "europe-west3-a" --machine-type "g1-small" --subnet "default" --maintenance-policy "MIGRATE" --image-family "debian-9" --image-project "debian-cloud" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "launcher-eu" --metadata 'startup-script=#!/bin/bash
 sudo su
 apt-get update
-apt-get install apache2-utils -y'
+apt-get install apache2-utils -y
+apt-get install git-core -y
+git clone https://github.com/favsto/intech-compute-balancing/
+rm -rf /usr/local/intech
+mv intech-compute-balancing/support/help/ /usr/local/intech
+rm -rf intech-compute-balancing/support/help/
+cd /usr/local/intech
+chmod +x *.sh'
 
 gcloud compute instances delete launcher-us --zone us-east4-a --quiet
 gcloud compute instances create "launcher-us" --description "This instance is for calling your services from America" --zone "us-east4-a" --machine-type "g1-small" --subnet "default" --maintenance-policy "MIGRATE" --image-family "debian-9" --image-project "debian-cloud" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "launcher-us" --metadata 'startup-script=#!/bin/bash
 sudo su
 apt-get update
-apt-get install apache2-utils -y'
+apt-get install apache2-utils -y
+apt-get install git-core -y
+git clone https://github.com/favsto/intech-compute-balancing/
+rm -rf /usr/local/intech
+mv intech-compute-balancing/support/help/ /usr/local/intech
+rm -rf intech-compute-balancing/support/help/
+cd /usr/local/intech
+chmod +x *.sh'
