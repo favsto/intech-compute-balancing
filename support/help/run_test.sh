c1a0405174001a4a02bdbf8f3df0b37f0dc48315
fausto_fusaro <fausto.fusaro@injenia.it>
2017-09-21 10:55:22 +0200

    improved comments into run_test.sh

--- support/help/run_test.sh
+++ support/help/run_test.sh
@@ -2,7 +2,8 @@
 
 if [ $# -eq 0 ]
   then
-    echo "You must specify at least your Load Balancer external IP address"
+    echo "You must specify at least your Load Balancer external IP address."
+    echo "Syntax: ./run_test <LB_IP_ADDRESS> [ <REQUESTS> <PARALLELISM> ]"
     exit 1
 fi
 
@@ -19,5 +20,5 @@ fi
 COLOR_YELLOW='\033[0;33m'
 COLOR_NONE='\033[0m' # No Color
 
-echo "${COLOR_YELLOW}Running test: $TEST_N requests with parallelism $TEST_C towards http://$TEST_URL/${COLOR_NONE}"
+echo -e "${COLOR_YELLOW}Running test: $TEST_N requests with parallelism $TEST_C towards http://$TEST_URL/${COLOR_NONE}"
 ab -n $TEST_N -c $TEST_C -s 60 -v 1 http://$TEST_URL/
