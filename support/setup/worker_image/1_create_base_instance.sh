41942d66444e41ee8f7ab96e133169505d054cc3
fausto_fusaro <fausto.fusaro@injenia.it>
2017-10-03 10:21:46 +0200

    fix wrong zone specification new architecture image

--- support/setup/worker_image/1_create_base_instance.sh
+++ support/setup/worker_image/1_create_base_instance.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # gcloud compute instances create "inpho-base" --zone "europe-west1-d" --machine-type "n1-standard-1" --image-family "debian-9" --image-project "debian-cloud" --boot-disk-size "10" --no-boot-disk-auto-delete --boot-disk-type "pd-ssd" --metadata "sql-connection-name=<CLOUD_SQL_CONNECTION_NAME>,destination-bucket=<OUTPUT_BUCKET_NAME>,sql-username=intech,sql-password=workshop2017"
-gcloud compute instances create "inpho-base" --zone "europe-west1-d" --machine-type "n1-standard-1" --image-family "debian-9" --image-project "debian-cloud" --boot-disk-size "10" --no-boot-disk-auto-delete --boot-disk-type "pd-ssd"
+gcloud compute instances create "inpho-base" --zone "europe-west3-a" --machine-type "n1-standard-1" --image-family "debian-9" --image-project "debian-cloud" --boot-disk-size "10" --no-boot-disk-auto-delete --boot-disk-type "pd-ssd"
